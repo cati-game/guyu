cae249f505b7d1274abcc7972e493620ab34e10d
cati-game <catiwilliam2703@gmail.com>
2025-06-28 00:28:24 +0200

    Update pages-index.js

--- pages-index.js
+++ pages-index.js
@@ -1,4 +1,6 @@
 const wikiPages = [
-    { title: "guyu balu", url: "file:///C:/Users/catiw/OneDrive/Skrivebord/this%20is%20a%20folder/guyu%20wiki/pages/guyu_balu.html" },
-    { title: "Template", url: "file:///C:/Users/catiw/OneDrive/Skrivebord/this%20is%20a%20folder/guyu%20wiki/pages/template.html" },
+  { title: "Guyu Balu", url: "pages/guyu_balu.html" },
+  { title: "Suyu Salu", url: "pages/suyu_salu.html" },
+  { title: "Martropolis", url: "pages/Martropolis.html" },
+  { title: "Suyu Lunatic First Edition", url: "pages/suyu_lunatic_1.html" },
 ];
