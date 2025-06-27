a81c07720b1bb261807defacac1de54faa8f803d
cati-game <catiwilliam2703@gmail.com>
2025-06-28 01:18:59 +0200

    Add files via upload

--- pages-index.js
+++ pages-index.js
@@ -1,6 +1,7 @@
 const wikiPages = [
     { title: "Guyu Balu", url: "https://cati-game.github.io/guyu/pages/guyu_balu.html" },
     { title: "Suyu Salu", url: "https://cati-game.github.io/guyu/pages/suyu_salu.html" },
+    { title: "Ouyu Oalu", url: "https://cati-game.github.io/guyu/pages/ouyu_oalu.html" },
+    { title: "Duck Duck Suy", url: "https://cati-game.github.io/guyu/pages/Duck_Duck_Suy.html" },    
     { title: "Suyu Lunatic First Edition", url: "https://cati-game.github.io/guyu/pages/suyu_lunatic_1.html" },
-    { title: "Template", url: "https://cati-game.github.io/guyu/pages/template.html" },
 ];
