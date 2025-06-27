5f282bfcb6202100aa41f007ea2df7ff28f62cc2
cati-game <catiwilliam2703@gmail.com>
2025-06-28 00:19:07 +0200

    Add files via upload

---
+++ pages-index.js
@@ -0,0 +1,4 @@
+const wikiPages = [
+    { title: "guyu balu", url: "file:///C:/Users/catiw/OneDrive/Skrivebord/this%20is%20a%20folder/guyu%20wiki/pages/guyu_balu.html" },
+    { title: "Template", url: "file:///C:/Users/catiw/OneDrive/Skrivebord/this%20is%20a%20folder/guyu%20wiki/pages/template.html" },
+];
