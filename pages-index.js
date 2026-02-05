837719e7f52510ae44adb152cebcb8c8f2bd17ae
William <will4943@zbc.dk>
2026-02-05 10:34:13 +0100

    v1.2 integration of new guys

--- pages-index.js
+++ pages-index.js
@@ -4,4 +4,6 @@ const wikiPages = [
     { title: "Ouyu Oalu", url: "https://cati-game.github.io/guyu/pages/ouyu_oalu.html" },
     { title: "Duck Duck Suy", url: "https://cati-game.github.io/guyu/pages/Duck_Duck_Suy.html" },    
     { title: "Suyu Lunatic First Edition", url: "https://cati-game.github.io/guyu/pages/suyu_lunatic_1.html" },
+    { title: "Ruyu Ralu", url: "https://cati-game.github.io/guyu/pages/ruyu_ralu.html" },
+    { title: "Luyu Lalu", url: "https://cati-game.github.io/guyu/pages/luyu_lalu.html" },
 ];
